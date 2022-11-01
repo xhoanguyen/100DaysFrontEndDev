edde30250e85d82cdf04db290fa87f1ef05ae391
xhoa <send2me2nd@gmail.com>
2022-11-01 23:32:59 +0100

    playing around with TS

--- TypeScript/01-helloworld/myhelloworld.js
+++ TypeScript/01-helloworld/myhelloworld.js
@@ -1,2 +1,16 @@
-console.log("Hello World");
-console.log("Coding God!!" + " Time for some fun");
+// ############# Variables #############
+var found = true;
+var grade = 88.6;
+var firstname = "Hoa";
+var lastName = "Nguyen";
+console.log("found");
+console.log(firstname + " " + lastName + " Grade is :" + grade);
+// using template string, REMEMBER to use backticks
+console.log("Hi ".concat(firstname));
+// ############# For Loops #############
+var total = 0;
+var reviews = [5, 5, 4.5, 1, 3];
+for (var i = 0; i < reviews.length; i++) {
+    total += reviews[i];
+}
+console.log("Averrage is " + total / reviews.length);
--- TypeScript/01-helloworld/myhelloworld.ts
+++ TypeScript/01-helloworld/myhelloworld.ts
@@ -1,2 +1,22 @@
-console.log("Hello World");
-console.log("Coding God!!" + " Time for some fun.");
+// ############# Variables #############
+
+let found: boolean = true;
+let grade: number = 88.6;
+let firstname: string = "Hoa";
+let lastName: string = "Nguyen";
+
+console.log("found");
+console.log(firstname + " " + lastName + " Grade is :" + grade);
+
+// using template string, REMEMBER to use backticks
+console.log(`Hi ${firstname}`);
+
+// ############# For Loops #############
+
+let total: number = 0;
+let reviews: number[] = [5, 5, 4.5, 1, 3];
+for (let i = 0; i < reviews.length; i++) {
+  total += reviews[i];
+}
+
+console.log("Averrage is " + total / reviews.length);
