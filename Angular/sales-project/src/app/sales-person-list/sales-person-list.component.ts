2cbb18c2e25a288e3548c759c92193e02382fa69
xhoa <send2me2nd@gmail.com>
2022-11-02 16:55:00 +0100

    added new sales project

---
+++ Angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css'],
+})
+export class SalesPersonListComponent implements OnInit {
+  // create an array of sales person objects
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('Hoa', 'Nguyen', 'Hoa.Nguyen@gmail.com', 11111),
+    new SalesPerson('Kasia', 'Nguyen', 'Kasia.Nguyen@gmail.com', 22222),
+    new SalesPerson('Linh', 'Nguyen', 'Linh.Nguyen@gmail.com', 33333),
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
